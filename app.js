026760c92bcb740ebd7a6b5133a235a49c644e55
Ailie McCorkindale <ailiem134@hotmail.com>
2022-01-24 16:12:03 +0100

    check win function added

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const p2score = document.getElementById("p2score");
 const p1arrow = document.getElementById("p1arrow");
 const p2arrow = document.getElementById("p2arrow");
 
+let squares = {
+  sq1: "",
+  sq2: "",
+  sq3: "",
+  sq4: "",
+  sq5: "",
+  sq6: "",
+  sq7: "",
+  sq8: "",
+  sq9: "",
+};
+
 class Player {
-    constructor(symbol) {
-        this.score = 0;
-        this.symbol = symbol;
-    }
+  constructor(symbol) {
+    this.score = 0;
+    this.symbol = symbol;
+  }
 }
 
 const player1 = new Player("x");
@@ -19,16 +31,35 @@ p2arrow.classList.add("arrow-hidden");
 p1score.innerHTML = player1.score;
 p2score.innerHTML = player2.score;
 
-
 const squareList = document.querySelectorAll(".square");
 
-squareList.forEach(square => {
-    square.addEventListener("click", () => {
-        square.textContent = currentPlayer.symbol;
-        square.clicked = true;
-        console.log(square, "clicked");
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-        console.log(square);
-    })
-})
-console.log(squareArray);
+squareList.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (square.textContent === "x" || square.textContent === "o") return;
+    square.textContent = currentPlayer.symbol;
+    squares[square.id] = currentPlayer.symbol;
+    changePlayer();
+    checkWin();
+  });
+});
+
+function changePlayer() {
+  currentPlayer = currentPlayer === player1 ? player2 : player1;
+  p1arrow.classList.toggle("arrow-hidden");
+  p2arrow.classList.toggle("arrow-hidden");
+}
+
+function checkWin() {
+    if((squares.sq1 !== "" && squares.sq1 === squares.sq2 && squares.sq1 === squares.sq3) || //top row
+    (squares.sq4 !== "" && squares.sq4 === squares.sq5 && squares.sq4 === squares.sq6) || // second row
+    (squares.sq7 !== "" && squares.sq7 === squares.sq8 && squares.sq7 === squares.sq9) || //third row
+    (squares.sq1 !== "" && squares.sq1 === squares.sq4 && squares.sq1 === squares.sq7) || //first column
+    (squares.sq2 !== "" && squares.sq2 === squares.sq5 && squares.sq2 === squares.sq8) || //second column
+    (squares.sq3 !== "" && squares.sq3 === squares.sq6 && squares.sq3 === squares.sq9) || //third column
+    (squares.sq1 !== "" && squares.sq1 === squares.sq5 && squares.sq1 === squares.sq9) || //first diagonal
+    (squares.sq3 !== "" && squares.sq3 === squares.sq5 && squares.sq3 === squares.sq7) //second diagonal 
+     ){
+        console.log("win!")
+        console.log(squares)
+    }
+}
