eaef0afdf7b91d0895eb8a989755cf1f1df5734e
Ailie McCorkindale <ailiem134@hotmail.com>
2022-01-24 16:26:16 +0100

    logic added for draw

--- app.js
+++ app.js
@@ -3,50 +3,64 @@ const p2score = document.getElementById("p2score");
 const p1arrow = document.getElementById("p1arrow");
 const p2arrow = document.getElementById("p2arrow");
 
-let squares = {
-  sq1: "",
-  sq2: "",
-  sq3: "",
-  sq4: "",
-  sq5: "",
-  sq6: "",
-  sq7: "",
-  sq8: "",
-  sq9: "",
-};
+let squares = {};
+
+
+let win = false;
 
 class Player {
-  constructor(symbol) {
+    constructor(symbol) {
     this.score = 0;
     this.symbol = symbol;
-  }
+}
 }
 
 const player1 = new Player("x");
 const player2 = new Player("o");
-
 let currentPlayer = player1;
 p2arrow.classList.add("arrow-hidden");
 
-p1score.innerHTML = player1.score;
-p2score.innerHTML = player2.score;
+function reset() {
+    squares = {
+        sq1: "",
+        sq2: "",
+        sq3: "",
+        sq4: "",
+        sq5: "",
+        sq6: "",
+        sq7: "",
+        sq8: "",
+        sq9: "",
+    };
+    
+    p1score.innerHTML = player1.score;
+    p2score.innerHTML = player2.score;
+    
+    squareList.forEach(square => {
+        square.textContent = "";
+    })
+}
+
 
 const squareList = document.querySelectorAll(".square");
 
 squareList.forEach((square) => {
-  square.addEventListener("click", () => {
-    if (square.textContent === "x" || square.textContent === "o") return;
-    square.textContent = currentPlayer.symbol;
-    squares[square.id] = currentPlayer.symbol;
-    changePlayer();
-    checkWin();
-  });
+    square.addEventListener("click", () => {
+        if (square.textContent === "x" || square.textContent === "o") return;
+        square.textContent = currentPlayer.symbol;
+        squares[square.id] = currentPlayer.symbol;
+        if(checkWin()) {
+            currentPlayer.score += 1;
+            reset();
+        } 
+        changePlayer();
+    });
 });
 
 function changePlayer() {
-  currentPlayer = currentPlayer === player1 ? player2 : player1;
-  p1arrow.classList.toggle("arrow-hidden");
-  p2arrow.classList.toggle("arrow-hidden");
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+    p1arrow.classList.toggle("arrow-hidden");
+    p2arrow.classList.toggle("arrow-hidden");
 }
 
 function checkWin() {
@@ -58,8 +72,15 @@ function checkWin() {
     (squares.sq3 !== "" && squares.sq3 === squares.sq6 && squares.sq3 === squares.sq9) || //third column
     (squares.sq1 !== "" && squares.sq1 === squares.sq5 && squares.sq1 === squares.sq9) || //first diagonal
     (squares.sq3 !== "" && squares.sq3 === squares.sq5 && squares.sq3 === squares.sq7) //second diagonal 
-     ){
+    ){
         console.log("win!")
         console.log(squares)
+        return true;
+    } else if(Object.values(squares).every(el => el !== "")) {
+        console.log("draw");
+        reset();
     }
+    return false;
 }
+
+window.addEventListener("load", reset());
