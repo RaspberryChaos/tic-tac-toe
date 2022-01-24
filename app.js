ff464308404d4e45dccc9d3227729c3571844f35
Ailie McCorkindale <ailiem134@hotmail.com>
2022-01-24 15:30:04 +0100

    event listener added to squares

--- app.js
+++ app.js
@@ -4,16 +4,31 @@ const p1arrow = document.getElementById("p1arrow");
 const p2arrow = document.getElementById("p2arrow");
 
 class Player {
-    constructor() {
+    constructor(symbol) {
         this.score = 0;
+        this.symbol = symbol;
     }
 }
 
-const player1 = new Player();
-const player2 = new Player();
+const player1 = new Player("x");
+const player2 = new Player("o");
 
 let currentPlayer = player1;
 p2arrow.classList.add("arrow-hidden");
 
 p1score.innerHTML = player1.score;
 p2score.innerHTML = player2.score;
+
+
+const squareList = document.querySelectorAll(".square");
+
+squareList.forEach(square => {
+    square.addEventListener("click", () => {
+        square.textContent = currentPlayer.symbol;
+        square.clicked = true;
+        console.log(square, "clicked");
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        console.log(square);
+    })
+})
+console.log(squareArray);
